849141ee0bfd592d5b315ef2299c164e6b1f67f6
Alexander-Steblyuk <steblyuk98@mail.ru>
2023-08-20 16:00:35 +0300

    rework code style of ExceptionController

--- src/main/java/com/example/users_and_rewards/UsersAndRewardsApplication.java
+++ src/main/java/com/example/users_and_rewards/UsersAndRewardsApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsersAndRewardsApplication{
-
+public class UsersAndRewardsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersAndRewardsApplication.class, args);
 	}
--- src/main/java/com/example/users_and_rewards/controllers/ExceptionController.java
+++ src/main/java/com/example/users_and_rewards/controllers/ExceptionController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExceptionController implements ErrorController {
+    private static final String STATUS_ATTRIBUTE_NAME = "status";
+    private static final String MESSAGE_ATTRIBUTE_NAME = "message";
+    private static final String ERROR_PAGE_PATH = "/error";
 
-    @RequestMapping("/error")
+    @RequestMapping(ERROR_PAGE_PATH)
     public String handle(Model model, HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
 
-        model.addAttribute("status", status);
-        model.addAttribute("message", message);
+        model.addAttribute(STATUS_ATTRIBUTE_NAME, status);
+        model.addAttribute(MESSAGE_ATTRIBUTE_NAME, message);
 
         return "error";
     }
